cfbd905c9c9f4be50c0029207a9460297d3c21f1
GAMMJ <gammjmj@gmail.com>
2026-02-27 14:49:26 +0900

    18일차 과제 해결

--- 필수과제/18일차/tailwind.config.js
+++ 필수과제/18일차/tailwind.config.js
@@ -1,9 +1,7 @@
-import daisyui from "daisyui"
-
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
 }
