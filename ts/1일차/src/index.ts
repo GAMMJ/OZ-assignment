6393b805b4fef03b14e6ec576dd8b24e30b19c8f
GAMMJ <gammjmj@gmail.com>
2026-04-02 15:07:31 +0900

    1일차 ts알아보기

---
+++ ts/1일차/src/index.ts
@@ -0,0 +1,23 @@
+let myString: string = "hello";
+console.log("String",myString);
+
+let myNumber: number = 123;
+console.log("Number",myNumber);
+
+let myBoolean: boolean = true;
+console.log("Boolean",myBoolean);
+
+let myNull: null = null;
+console.log("Null",myNull);
+
+let myAny: any = "hello";
+console.log("Any",myAny);
+
+myAny = 123;
+console.log("Any",myAny);
+
+myAny = true;
+console.log("Any",myAny);
+
+myAny = null;
+console.log("Any",myAny);
